7733d26ab1f474f3583d28de345c6ee925d48b53
Minor Gordon <minorg@server.fake>
2024-03-19 16:09:13 -0400

    interchange graph asset test

--- agrovoc/assets/interchange_graph.py
+++ agrovoc/assets/interchange_graph.py
@@ -1,7 +1,8 @@
-from dagster import asset
+from dagster import asset, get_dagster_logger
 from graphs2go.resources.oxigraph_config import OxigraphConfig
 from graphs2go.models import interchange
 from rdflib import URIRef
+from tqdm import tqdm
 
 from agrovoc.transform_thesaurus_to_interchange_models import (
     transform_thesaurus_to_interchange_models,
@@ -15,13 +16,26 @@ from agrovoc.releases_partitions_definition import releases_partitions_definitio
 def interchange_graph(
     oxigraph_config: OxigraphConfig, release_graph: ReleaseGraph
 ) -> interchange.Graph.Descriptor:
+    logger = get_dagster_logger()
+
     with interchange.Graph.create(
         oxigraph_config=oxigraph_config,
         identifier=URIRef("http://aims.fao.org/aos/agrovoc/"),
     ) as interchange_graph:
+        if not interchange_graph.is_empty:
+            logger.info("interchange graph is not empty, skipping load")
+            return interchange_graph.descriptor
+
+        logger.info("loading interchange graph")
+
         thesaurus = Thesaurus(graph=release_graph.to_rdflib_graph())
 
-        for model in transform_thesaurus_to_interchange_models(thesaurus=thesaurus):
+        for model in tqdm(
+            transform_thesaurus_to_interchange_models(thesaurus=thesaurus),
+            desc="interchange models",
+        ):
             interchange_graph.add(model)
+
+        logger.info("loaded interchange graph")
 
         return interchange_graph.descriptor
--- tests/agrovoc_test/conftest.py
+++ tests/agrovoc_test/conftest.py
@@ -1,15 +1,17 @@
 from collections.abc import Iterable
 import logging
 from pathlib import Path
-import oxrdflib
-import pyoxigraph
 import pytest
+from graphs2go.models import interchange
 from graphs2go.utils.configure_markus import configure_markus
 from graphs2go.resources.oxigraph_config import OxigraphConfig
 from graphs2go.utils.load_dotenv import load_dotenv
-from rdflib import ConjunctiveGraph, Graph
+from rdflib import Graph
 
 from agrovoc.assets.release_graph import release_graph as release_graph_asset
+from agrovoc.assets.interchange_graph import (
+    interchange_graph as interchange_graph_asset,
+)
 from agrovoc.models.release import Release
 from agrovoc.models.release_graph import ReleaseGraph
 from agrovoc.models.thesaurus import Thesaurus
@@ -19,6 +21,16 @@ from agrovoc.utils.find_releases import find_releases
 load_dotenv()
 configure_markus()
 logger = logging.getLogger(__name__)
+
+
+@pytest.fixture(scope="session")
+def interchange_graph(
+    oxigraph_config: OxigraphConfig, release_graph: ReleaseGraph
+) -> interchange.Graph:
+    descriptor: interchange.Graph.Descriptor = interchange_graph_asset(
+        oxigraph_config=oxigraph_config, release_graph=release_graph
+    )  # type: ignore
+    return interchange.Graph.open(descriptor=descriptor)
 
 
 @pytest.fixture(scope="session")
