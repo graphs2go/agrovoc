80f9e6f9ad31cf82646095534ac0431a54ce627a
Minor Gordon <minorg@server.fake>
2024-03-19 10:43:40 -0400

    code to build Oxigraph from nt file

--- tests/agrovoc_test/conftest.py
+++ tests/agrovoc_test/conftest.py
@@ -1,4 +1,5 @@
 from collections.abc import Iterable
+import logging
 from pathlib import Path
 import pytest
 from graphs2go.utils.configure_markus import configure_markus
@@ -13,6 +14,7 @@ from agrovoc.utils.find_releases import find_releases
 
 load_dotenv()
 configure_markus()
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="session")
@@ -32,15 +34,38 @@ def release_config() -> ReleaseConfig:
 
 @pytest.fixture(scope="session")
 def release_graph(oxigraph_config: OxigraphConfig, release: Release) -> Iterable[Graph]:
-    import oxrdflib
+    import oxrdflib  # noqa: F401
 
     graph = Graph(store="Oxigraph")
-    graph.open(
-        str(oxigraph_config.parse().directory_path / release.version.isoformat())
+    oxigraph_dir_path = (
+        oxigraph_config.parse(
+            directory_path_default=Path(__file__).parent.parent.parent
+            / "data"
+            / "oxigraph"
+        ).directory_path
+        / "agrovoc"
+        / release.version.isoformat()
     )
+    oxigraph_dir_path.mkdir(parents=True, exist_ok=True)
+    graph.open(str(oxigraph_dir_path))
     try:
-        if not graph:
+        if len(graph) > 0:
+            logger.info(
+                "reuising existing Oxigraph %s with %d triples",
+                oxigraph_dir_path,
+                len(graph),
+            )
+        else:
+            logger.info(
+                "building %s Oxigraph from %s", oxigraph_dir_path, release.nt_file_path
+            )
             graph.parse(source=release.nt_file_path)
+            logger.info(
+                "built %s Oxigraph from %s with %d triples",
+                oxigraph_dir_path,
+                release.nt_file_path,
+                len(graph),
+            )
 
         yield graph
 
