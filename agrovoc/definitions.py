caa18c4c7b5703d34e2e87d7f15d752efadd11a4
Minor Gordon <minorg@server.fake>
2024-03-21 19:50:16 -0400

    stubbing out jobs

--- agrovoc/definitions.py
+++ agrovoc/definitions.py
@@ -4,13 +4,13 @@ from graphs2go.resources.output_config import OutputConfig
 from graphs2go.resources.rdf_store_config import RdfStoreConfig
 from graphs2go.utils.configure_markus import configure_markus
 from graphs2go.utils.load_dotenv import load_dotenv
-from graphs2go.assets.build_skos_files_asset import build_skos_files_asset
-from graphs2go.assets.build_skos_graph_asset import build_skos_graph_asset
 
-from agrovoc.releases_partitions_definition import releases_partitions_definition
+from agrovoc.assets.skos_files import skos_files
+from agrovoc.assets.skos_graph import skos_graph
 from agrovoc.assets.interchange_graph import interchange_graph
 from agrovoc.assets.release import release
 from agrovoc.assets.release_graph import release_graph
+from agrovoc.jobs.skos_files import skos_files as skos_files_job
 from agrovoc.resources.release_config import ReleaseConfig
 
 configure_markus()
@@ -22,9 +22,10 @@ definitions = Definitions(
         interchange_graph,
         release,
         release_graph,
-        build_skos_graph_asset(partitions_def=releases_partitions_definition),
-        build_skos_files_asset(partitions_def=releases_partitions_definition),
+        skos_files,
+        skos_graph,
     ],
+    jobs=[skos_files_job],
     resources={
         "output_config": OutputConfig.from_env_vars(
             directory_path_default=Path(__file__).parent.parent / "data" / "output"
