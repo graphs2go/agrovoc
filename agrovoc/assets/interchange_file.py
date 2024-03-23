5650941a94898959a1907082c7ba9b57f37952d0
Minor Gordon <minorg@server.fake>
2024-03-23 12:48:32 -0400

    output multiple RDF formats

--- agrovoc/assets/interchange_file.py
+++ agrovoc/assets/interchange_file.py
@@ -1,7 +1,9 @@
 from graphs2go.assets.build_interchange_file_asset import build_interchange_file_asset
+from graphs2go.models import rdf
 
 from agrovoc.releases_partitions_definition import releases_partitions_definition
 
 interchange_file = build_interchange_file_asset(
-    partitions_def=releases_partitions_definition
+    partitions_def=releases_partitions_definition,
+    rdf_formats=(rdf.Format.NTRIPLES, rdf.Format.TURTLE),
 )
