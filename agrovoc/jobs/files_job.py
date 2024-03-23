98fce189ac80f41992eb3aafa595af3f30b3df9d
Minor Gordon <minorg@server.fake>
2024-03-23 11:58:09 -0400

    working on files

--- agrovoc/jobs/files_job.py
+++ agrovoc/jobs/files_job.py
@@ -4,7 +4,7 @@ from agrovoc.assets.interchange_file import interchange_file as interchange_file
 from agrovoc.assets.skos_file import skos_file as skos_file_asset
 
 files_job = define_asset_job(
-    "file_job",
+    "files_job",
     selection=[
         "*" + interchange_file_asset.key.path[0],
         "*" + skos_file_asset.key.path[0],
