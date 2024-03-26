d1d9935dbfae1838238480a1b0a0e4a4c0a4f617
Minor Gordon <minorg@server.fake>
2024-03-26 15:54:48 -0400

    add more read_only=True

--- tests/agrovoc_test/assets/interchange_graph_test.py
+++ tests/agrovoc_test/assets/interchange_graph_test.py
@@ -10,5 +10,5 @@ def test_asset(rdf_store_config: RdfStoreConfig, release_graph: ReleaseGraph) ->
         rdf_store_config=rdf_store_config, release_graph=release_graph
     )
     assert isinstance(asset, interchange.Graph.Descriptor)
-    with interchange.Graph.open(descriptor=asset) as interchange_graph_:
+    with interchange.Graph.open(descriptor=asset, read_only=True) as interchange_graph_:
         assert not interchange_graph_.is_empty
